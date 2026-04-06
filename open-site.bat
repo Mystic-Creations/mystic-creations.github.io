0186861dff4846015ccda6192d800cb9f4ca0460
JustMili <rodx160@gmail.com>
2026-04-06 21:28:47 +0200

    Forgot about the starting

--- open-site.bat
+++ open-site.bat
@@ -3,10 +3,10 @@ title Dev Server
 
 if exist C:\Users\%username%\.deno\bin\deno.exe (
    deno task dev --port 2137
+   start http://localhost:2137
 ) else (
    powershell -command "irm https://deno.land/install.ps1 | iex"
    deno install
    deno task dev --port 2137
+   start http://localhost:2137
 )
-
-pause
