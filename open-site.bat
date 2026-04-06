a06f57d5635b04d03f6051dcd213c413c521d62e
JustMili <rodx160@gmail.com>
2026-04-06 21:26:05 +0200

    more robust open-site.bat

--- open-site.bat
+++ open-site.bat
@@ -1,2 +1,12 @@
-deno task dev --port 2137
-start http://localhost:2137
+@echo off
+title Dev Server
+
+if exist C:\Users\%username%\.deno\bin\deno.exe (
+   deno task dev --port 2137
+) else (
+   powershell -command "irm https://deno.land/install.ps1 | iex"
+   deno install
+   deno task dev --port 2137
+)
+
+pause
