53aea6aef508a90dc92705edd0f41a27f4400c12
Tom Boddaert <70723965+tomBoddaert@users.noreply.github.com>
2026-04-13 11:59:52 +0100

    Updated site URL after repo name change

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lumynitystudios.github.io",
+  site: "https://lumynity-studios.github.io",
   build: {
     format: 'preserve',
   },
